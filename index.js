d0d02562e37eb8cadfcd2b6e2267f4f89a132bb0
Evan Chen <evan@Evans-MacBook-Pro.local>
2018-02-11 20:53:32 -0800

    commented out stuff

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const serverJS = require('engine.io/lib/server.js')
  
-// Dependencies
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
-var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
-app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/static'));
-// Routing
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
-});
-// Starts the server.
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
-});
+// // Dependencies
+// var express = require('express');
+// var http = require('http');
+// var path = require('path');
+// var socketIO = require('socket.io');
+// var app = express();
+// var server = http.Server(app);
+// var io = socketIO(server);
+// app.set('port', 5000);
+// app.use('/static', express.static(__dirname + '/static'));
+// // Routing
+// app.get('/', function(request, response) {
+//   response.sendFile(path.join(__dirname, 'index.html'));
+// });
+// // Starts the server.
+// server.listen(5000, function() {
+//   console.log('Starting server on port 5000');
+// });
