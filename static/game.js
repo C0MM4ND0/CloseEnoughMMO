8884fb96bc5f79b9ca6d69cc6cd8f738e84ea25c
Evan Chen <evan@Evans-MacBook-Pro.local>
2018-02-11 20:47:22 -0800

    files added

---
+++ static/game.js
@@ -0,0 +1,7 @@
+
+
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d")
+
+context.font = "15px Comic Sans MS";
+context.fillText("Evan has good chi",canvas.width*.3,canvas.height*.5);
