07c2ed10d9f64c2ddccd39fc43ce9b93c1359c76
Evan Chen <evan@Evans-MacBook-Pro.local>
2018-02-11 23:23:38 -0800

    added stuff

--- index.js
+++ index.js
@@ -15,32 +15,160 @@ app.set('port', PORT);
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+	response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
 server.listen(PORT, function() {
-  console.log('Starting server on port ' +  PORT);
+	console.log('Starting server on port ' +  PORT);
 });
+
+
+
 
 
 //SERVER SIDE CODE
+class User{
+	constructor(id, x, y, dx, dy, name){
+		this.id = id
+		this.x = x
+		this.y = y
+		this.dx = dx
+		this.dy = dy
+		this.name = name
+	}
+
+	move(){
+
+		if(!(this.x < 0 && this.dx < 0 || this.x + dimW*.1 > dimW && this.dx > 0)){
+			this.x += this.dx
+		}
+
+		if(!(this.y < 0 && this.dy < 0 || this.y + dimH*.1 > dimH && this.dy > 0)){
+			this.y += this.dy
+		}
+
+
+	}
+
+}
+
+//GLOBAL VARS
+
+//List of current users
+var users = []
+var serverLoop
+var serverBroadcast 
+
+var dimW
+var dimH
+
+
+const MAX_FPS = 60
+
+//RUNNING EACH INSTANCE FOR EVERY USER
 io.on('connection', function(socket) {
+	
+	socket.on('new user', function(package){
+		verbose(socket.id + ' has joined ')
+		addUser(socket.id, package.username, package.x, package.y)
 
-	//RUNNING EACH INSTANCE FOR EVERY USER
-	//READERS
+		if(dimW == null){ dimW = package.dimW}
+		if(dimH == null){ dimH = package.dimH}
 
-	//WRITERS
+			verbose(dimW + ' ' + dimH)
+
+	})
 
 
+	socket.on('disconnect', function() {
+		verbose(socket.id + ' disconnected ')
+		removeUser(socket.id)
+	})
+
+	socket.on('moveX', function(package){
+		if(users.length == 0){ return }
+		var user  = users[getUserIndex(socket.id)]
+		if(!package.release){ //go
+			user.dx = (package.positive) ? 5 : -5
+		}else{//stop
+			user.dx = 0
+		}
+	})
+
+	socket.on('moveY', function(package){
+		if(users.length == 0){ return }
+		var user  = users[getUserIndex(socket.id)]
+		if(!package.release){ //go
+			user.dy = (package.positive) ? 5 : -5
+		}else{//stop
+			user.dy = 0
+		}
+	})
 
 });
 
-setInterval(function() {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
 
+function updateClients(){
+	 io.sockets.emit('update', users);
+}
+
+
+function manageClients(){ //this is where all the server stuff is done
+	moveUsers()
+}
+
+
+
+
+function moveUsers(){
+	for(var i = 0; i < users.length; i++){
+		var user = users[i]
+		user.move()
+	}
+}
+
+
+
+function start(){
+	serverLoop = setInterval(manageClients, 1000/ MAX_FPS)
+	serverBroadcast = setInterval(updateClients, 1000 / MAX_FPS);
+}
+
+
+function addUser(id, name, x, y){
+	users.push(new User(id, x , y, 0, 0, name))
+}
+
+function getUserIndex(id){
+	for(var i = 0; i < users.length; i++){
+		if(users[i].id == id){
+			return i;
+		}
+	}
+}
+
+
+function removeUser(id){
+	for(var i = 0; i < users.length; i++){
+		if(users[i].id == id){
+			users.splice(i,1)
+			return;
+		}
+	}
+}
+
+function printUsers(){
+	for(var i = 0; i < users.length; i++){
+		var u = users[i]
+		verbose(u.name)
+	}
+}
 
 //MISC FUNCTIONS
-function verbosea(args){
+function verbose(args){ //simple verbose
 	console.log(args)
 }
+
+
+start()
+
--- static/game.js
+++ static/game.js
@@ -3,28 +3,143 @@
 window.onload = function(){ //window load guard
 
 
-//SOCKET OBSERVERS
-var socket = io()
-socket.on('message', function(data) {
 
-});
+
 //GLOBAL VARS
 const canvas = document.getElementById("canvas")
 const context = canvas.getContext("2d")
+const MAX_FPS = 60
+var gameLoop
+
+
+var users //client side
 
 
 
 
+//SOCKET OBSERVERS
+var socket = io()
+socket.on('update', function(data) {
+	update(data) //data = users
+});
+
+//SOCKET EMITTERS
+//CLIENT PROMPTS
+var name = prompt("Enter name ya");
+if (name == null || name == "") {
+	name = "Anon"+Math.floor(Math.random()*100);
+	socket.emit('new user', {username: name, x: canvas.width/2, y: canvas.height/2, dimW: canvas.width, dimH: canvas.height})
+
+}else{
+	verbose(name)
+	socket.emit('new user', {username: name, x: canvas.width/2, y: canvas.height/2, dimW: canvas.width, dimH: canvas.height})
+}
+
+
+//USER KEYS
+window.addEventListener("keydown", function (event) {
+	if (event.defaultPrevented) {
+    //return; 
+}
+switch(event.key){
+	case "a":
+	socket.emit('moveX', {release: false, positive: false})
+	break;
+	case "d":
+	socket.emit('moveX', {release: false, positive: true})
+	break;
+	case "w":
+	socket.emit('moveY', {release: false, positive: false})
+	break
+	case "s":
+	socket.emit('moveY', {release: false, positive: true})
+	break
+	default:
+	break;
+}
+
+
+event.preventDefault();
+}, true);
+
+window.addEventListener("keyup", function (event) {
+	if (event.defaultPrevented) {
+    //return; 
+}
+switch(event.key){
+	case "a":
+	socket.emit('moveX', {release: true, positive: false})
+	break;
+	case "d":
+	socket.emit('moveX', {release: true, positive: true})
+	break;
+	case "w":
+	socket.emit('moveY', {release: true, positive: false})
+	break
+	case "s":
+	socket.emit('moveY', {release: true, positive: true})
+	break
+	default:
+	break;
+}
+
+
+event.preventDefault();
+}, true);
+
+
+
+
+function gameLoop(){
+	draw()
+}
+
+
+function update(data){ //update sprites client side
+	users = data
+}
+
+
+function draw(){ //draw client
+
+	context.fillStyle="#FFFFFF"; //prompting for a new paint over
+	//coloring backgrounds
+	context.fillRect(0,0,canvas.width,canvas.height)
+
+	//drawing users
+	for(index in users){
+		var user = users[index]
+		context.fillStyle="#000000"; 
+		context.fillRect(user.x,user.y,canvas.width*.05,canvas.height*.05)
+		context.font = "15px Comic Sans MS";
+		context.fillText(user.name,user.x, user.y);
+	}
+}
+
+
+function start(){
+	gameLoop = setInterval(gameLoop, 1000/MAX_FPS)
+}
+
+
+
+//MISC function
+
+function verbose(args){
+	console.log(args)
+}
 
 //observer for managing the size of the canvas
-function resizeCanvas() {
-	canvas.style.width = window.innerWidth + "px";
-	setTimeout(function() {
-		canvas.style.height = window.innerHeight + "px";
-	}, 0);
-};
-window.onresize = resizeCanvas;
-//manual pack of canvas	
-resizeCanvas();
+// function resizeCanvas() {
+// 	canvas.style.width = window.innerWidth + "px";
+// 	setTimeout(function() {
+// 		canvas.style.height = window.innerHeight + "px";
+// 	}, 0);
+// };
+// window.onresize = resizeCanvas;
+// //manual pack of canvas	
+// resizeCanvas();
+
+start()
 
 }
